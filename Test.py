e846e0f3e98c80b3fa906e7972b80d61799924c1
Yami <sbotirzonov@gmail.com>
2025-07-11 17:02:28 +0500

    Не большие улучшения!

--- Test.py
+++ Test.py
@@ -4,7 +4,60 @@ def file_saver(tasks):
             file.write(task + "\n")
     print("Задачи сохранены!")
 
+def add_task(tasks):
+    text = input("Введите задачу: ").strip()
+    if text:
+        tasks.append("✘ " + text)
+        print("Задача добавлена!")
+    else:
+        print("Задание не может быть пустым!")
+        print()
+    return tasks
 
+def list_task(tasks):
+    if tasks:
+        for i, t in enumerate(tasks, start=1):
+            print(f"{i}. {t}")
+    else:
+        print("Список задач пуст!")
+        print()
+
+
+def doner_task(tasks):
+    if not tasks:
+        print("Сначала добавьте задания!")
+        print()
+    else:
+        try:
+            n = int(input("Введите номер задачи: "))
+            if 1 <= n <= len(tasks):
+                if tasks[n - 1].startswith("✘ "):
+                    tasks[n - 1] = tasks[n - 1].replace("✘ ", "✔ ")
+                    print("Задача помечена как выполненная!")
+                else:
+                    print("Задача уже выполнена!")
+            else:
+                print("Нет такой задачи!")
+        except ValueError:
+            print("Введите число!")
+    return tasks
+
+
+def delete_task(tasks):
+    if not tasks:
+        print("Список пуст!")
+        print()
+    else:
+        try:
+            n = int(input("Введите номер задачи: "))
+            if 1 <= n <= len(tasks):
+                deleted = tasks.pop(n - 1)
+                print(f"Удалена задача: {deleted}")
+            else:
+                print("Нет такой задачи!")
+        except ValueError:
+            print("Введите число!")
+    return tasks
 
 tasks = []
 try:
@@ -15,64 +68,46 @@ except FileNotFoundError:
     print("Файл задач не найден. Начинаем с пустого списка.")
 
 while True:
-    print("Команды: add, list, done, delete, save, exit")
+    print("Команды: add, list, done, delete, save, exit, help")
     cmd = input(">>> ").strip().lower()
 
     if cmd == "exit":
+        if len(tasks) > 0:
+            file_saver(tasks)
         print("Пока!")
         break
 
     elif cmd == "add":
-        text = input("Введите задачу: ").strip()
-        if text:
-            tasks.append("✘ " + text)
-            print("Задача добавлена!")
-        else:
-            print("Задание не может быть пустым!")
+        tasks = add_task(tasks)
 
     elif cmd == "list":
-        if tasks:
-            for i, t in enumerate(tasks, start=1):
-                print(f"{i}. {t}")
-        else:
-            print("Список задач пуст!")
+        list_task(tasks)
 
     elif cmd == "done":
-        if not tasks:
-            print("Сначала добавьте задания!")
-        else:
-            try:
-                n = int(input("Введите номер задачи: "))
-                if 1 <= n <= len(tasks):
-                    if tasks[n-1].startswith("✘ "):
-                        tasks[n-1] = tasks[n-1].replace("✘ ", "✔ ")
-                        print("Задача помечена как выполненная!")
-                    else:
-                        print("Задача уже выполнена!")
-                else:
-                    print("Нет такой задачи!")
-            except ValueError:
-                print("Введите число!")
+        tasks = doner_task(tasks)
 
     elif cmd == "delete":
-        if not tasks:
-            print("Список пуст!")
-        else:
-            try:
-                n = int(input("Введите номер задачи: "))
-                if 1 <= n <= len(tasks):
-                    deleted = tasks.pop(n-1)
-                    print(f"Удалена задача: {deleted}")
-                else:
-                    print("Нет такой задачи!")
-            except ValueError:
-                print("Введите число!")
+        tasks = delete_task(tasks)
 
     elif cmd == "save":
         file_saver(tasks)
 
+    elif cmd == "help":
+        print("""
+Добро пожаловать в To-Do менеджер!
+Команды:
+add     - Добавить задачу
+list    - Показать все задачи
+done    - Отметить задачу выполненной
+delete  - Удалить задачу
+save    - Сохранить задачи в файл
+exit    - Выход
+""")
+        print()
     else:
         print("Неизвестная команда.")
+
+
 
 
 
